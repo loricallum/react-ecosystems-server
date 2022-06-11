c1fdbc4422a81bd0c64db8f2360e697ee77e0db2
Lori C <lorimlewis@gmail.com>
2022-06-11 23:23:46 +0100

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+    <div className="App">
+            <h1> Let's learn Polish!</h1>
+
+
+    </div>
+);
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+//need to set up webpack to build and install the project
